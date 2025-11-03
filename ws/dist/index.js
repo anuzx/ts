b7245c6ce607ba4d14f05cfb39a13be2079ce926
anuzx <anujparasar4266@gmail.com>
2025-11-04 01:02:53 +0530

    ws

---
+++ ws/dist/index.js
@@ -0,0 +1,16 @@
+import { WebSocketServer } from "ws";
+const wss = new WebSocketServer({ port: 8080 });
+//server event handler
+wss.on("connection", function (socket) {
+    console.log("socket connected");
+    // setInterval(() => {
+    //  socket.send("hello");
+    // }, 500);
+    //client handler
+    socket.on("message", (e) => {
+        if (e.toString() == "ping") {
+            socket.send("pong");
+        }
+    });
+});
+//# sourceMappingURL=index.js.map
